0639a56b95bd4c3c8d5e5aab8871de16e45a7ca6
Kevin Gomes <kevinmg50@gmail.com>
2023-03-01 15:58:11 -0300

    feat: implemented system to run the application and error handling when running

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"find-server/app"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Iniciando")
+
+	application := app.Generate()
+	if error := application.Run(os.Args); error != nil {
+		log.Fatal(error)
+	}
 }
