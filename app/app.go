822eca8fbf30bf9711fce9a0385e70cf194afd16
Kevin Gomes <kevinmg50@gmail.com>
2023-03-01 16:06:34 -0300

    feat: implemented functionality that searches the public ip and returns in the terminal

--- app/app.go
+++ app/app.go
@@ -1,11 +1,44 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação em Linha de Comando"
 	app.Usage = "Busca de IP e nome de servidores"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IP de servidores",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "kevindev.com.br",
+				},
+			},
+			Action: findIp,
+		},
+	}
+
 	return app
+}
+
+func findIp(c *cli.Context) {
+	host := c.String("host")
+
+	ips, error := net.LookupIP(host)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
